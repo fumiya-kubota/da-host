d2a56d78cdbf59e25dc870b49153022035747e48
fumiya-kubota <fumiya.kubota@glucose.jp>
2017-10-25 22:07:10 +0900

    initial commit

---
+++ src/js/components/main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Main extends Component {
+  render () {
+    return (
+      <div>
+        This is Main Component
+      </div>
+    )
+  }
+}
+
+export default Main
